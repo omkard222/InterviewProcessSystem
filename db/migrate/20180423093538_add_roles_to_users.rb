1138633a9e7bef26318221e4f3bf72f08493003c
Administrator <administrator@adminisorsimac2.igglobal.com>
2018-05-08 11:56:01 +0530

    some changes

--- db/migrate/20180423093538_add_roles_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddRolesToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :admin_role, :boolean, default: false
-    add_column :users, :manager_role, :boolean, default: false
-    add_column :users, :hr_role, :boolean, default: false
-    add_column :users, :interviewer_role, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -107,6 +107,8 @@ ActiveRecord::Schema.define(version: 20180427110948) do
     t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
@@ -148,6 +150,7 @@ ActiveRecord::Schema.define(version: 20180427110948) do
     t.integer "employee_id"
     t.string "designation"
     t.string "phone_no"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
